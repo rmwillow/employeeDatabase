4593ca5a37c26590bde99f273b4034af631a4d9b
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-07-21 11:13:20 -0500

    install more npm packages

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department(department_name)
+VALUES("Engineering"), ("Sales"), ("Finance"), ("Legal");
+
+INSERT INTO role(title, salary, department_id)
+VALUES("Engineer", 90000, 1), ("Senior Engineer", 120000, 1);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 1, 2), ('Charlotte', 'Crow', 1, null), ('Mike', 'Seaux', 1, 2), ('Jimmy', 'Jones', 2, 2);
