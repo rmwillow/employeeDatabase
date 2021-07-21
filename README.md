68234c0b0aaee46f6f3fa6962ac4c076a303d0f4
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-07-21 11:23:35 -0500

    inq prompt & chalk decor added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # employeeDatabase
+npm i inquirer
+npm i console.table
+npm i figlet
+npm i chalk
+npm i nodemon
+npm i validator
+npm i express
+npm i dotenv@8.2.0
---
+++ server.js
@@ -0,0 +1,113 @@
+// Dependencies
+const connection = require('./utils/config');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const view = require('./utils/view');
+const add = require('./utils/add');
+const remove = require('./utils/remove');
+const update = require('./utils/update');
+const figlet = require('figlet');
+
+// Connecting to DB
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(
+    chalk.greenBright(
+      `====================================================================================`
+    )
+  );
+  console.log(``);
+  console.log(
+    chalk.blueBright(figlet.textSync('Employee Tracker'))
+  );
+  console.log(``);
+  console.log(`                                                         ` + chalk.whiteBright.bgCyan.bold('Created By: Rachel McGrath'));
+  console.log(
+    chalk.green(
+      `====================================================================================`
+    )
+  );
+  init();
+});
+
+
+// Set Menu Options
+const menuOptions = [
+    'View All Employees',
+    'View All Employees By Manager',
+    'View All Employees By Department',
+    'Add Employee',
+    'Remove Employee',
+    'Update Employee Role',
+    'Update Employee Manager',
+    'View All Roles',
+    'Add Role',
+    'Remove Role',
+    'Add Department',
+    'View Department Budget',
+    'Remove Department',
+    'Exit'
+  ];
+  
+  // Start Inquirer
+  const init = () => {
+    console.log(``);
+    inquirer
+      .prompt([
+        {
+          name: 'selection',
+          type: 'list',
+          message: 'What would you like to do?',
+          choices: menuOptions
+        }
+      ])
+      .then((answer) => {
+        switch (answer.selection) {
+          case 'View All Employees':
+            view.viewAllEmployees();
+            break;
+          case 'View All Employees By Manager':
+            view.viewEmployeesByManager();
+            break;
+          case 'View All Employees By Department':
+            view.viewEmployeesByDept();
+            break;
+          case 'Add Employee':
+            add.addEmployee();
+            break;
+          case 'Remove Employee':
+            remove.removeEmployee();
+            break;
+          case 'Update Employee Role':
+            update.updateEmployeeRole();
+            break;
+          case 'Update Employee Manager':
+            update.updateEmployeeManager();
+            break;
+          case 'View All Roles':
+            view.viewAllRoles();
+            break;
+          case 'Add Role':
+            add.addRole();
+            break;
+          case 'Remove Role':
+            remove.removeRole();
+            break;
+          case 'Add Department':
+            add.addDept();
+            break;
+          case 'View Department Budget':
+            view.viewDeptBudget();
+            break;
+          case 'Remove Department':
+            remove.removeDept();
+            break;
+          case 'Exit':
+            connection.end();
+            break;
+        }
+      });
+  };
+  
+  // Export init function to be used in utils
+  exports.init = init;
